8e4d21ed5ae51c290eaefab98a0f75afe09d3eb2
Kamil <kamciu9@gmail.com>
2020-08-06 00:08:00 +0200

    second commit

--- map1.py
+++ map1.py
@@ -2,6 +2,7 @@ import folium
 import pandas as pd
 import random
 import numpy as np
+from branca.element import Template, MacroElement
 
 data=pd.read_csv("Volcanoes_USA.txt")
 
@@ -13,7 +14,7 @@ def color_maker(elevation):
     else:
         return 'red'
 
-map = folium.Map( location=[48.853, 2.35], zoom_start=6,tiles='cartodbdark_matter')
+map = folium.Map( location=[48.853, 2.35], zoom_start=6, tiles='cartodbdark_matter')
 
 fg = folium.FeatureGroup(name="My Map")
 
@@ -32,15 +33,113 @@ for lt,ln,el in zip(latitude,longitude,elevation):
 fgp=folium.FeatureGroup(name="Population")
 
 fgp.add_child(folium.GeoJson(data=open('world.json','r',encoding='utf-8-sig').read(),
-                             style_function=lambda x: {'fillColor':'blue' if x['properties']['POP2005']<10000000
-                                                      else 'green' if 10000000 <= x['properties']['POP2005']<20000000
+                             style_function=lambda x: {'fillColor':'blue' if x['properties']['POP2005']<9000000
+                                                      else 'green' if 9000000 <= x['properties']['POP2005']<20000000
                                                       else 'yellow' if 20000000<= x['properties']['POP2005']<100000000
                                                       else 'red'}))
 
 
+
+template = """
+{% macro html(this, kwargs) %}
+
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Map</title>
+  <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+
+  <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+  <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+
+  <script>
+  $( function() {
+    $( "#maplegend" ).draggable({
+                    start: function (event, ui) {
+                        $(this).css({
+                            right: "auto",
+                            top: "auto",
+                            bottom: "auto"
+                        });
+                    }
+                });
+});
+
+  </script>
+</head>
+<body>
+
+
+<div id='maplegend' class='maplegend'
+    style='position: absolute; z-index:9999; border:2px solid grey; background-color:rgba(255, 255, 255, 0.8);
+     border-radius:6px; padding: 10px; font-size:14px; right: 20px; bottom: 20px;'>
+
+<div class='legend-title'>Population size</div>
+<div class='legend-scale'>
+  <ul class='legend-labels'>
+    <li><span style='background:red;opacity:0.7;'></span>Big (more than 100 million people)</li>
+    <li><span style='background:orange;opacity:0.7;'></span>Medium (between 20 and 100 million people)</li>
+    <li><span style='background:green;opacity:0.7;'></span>Small (between 10 and 20 million people)</li>
+    <li><span style='background:blue;opacity:0.7;'></span>Very Small (less than 10 million people)</li>
+
+  </ul>
+</div>
+</div>
+
+</body>
+</html>
+
+<style type='text/css'>
+  .maplegend .legend-title {
+    text-align: left;
+    margin-bottom: 5px;
+    font-weight: bold;
+    font-size: 90%;
+    }
+  .maplegend .legend-scale ul {
+    margin: 0;
+    margin-bottom: 5px;
+    padding: 0;
+    float: left;
+    list-style: none;
+    }
+  .maplegend .legend-scale ul li {
+    font-size: 80%;
+    list-style: none;
+    margin-left: 0;
+    line-height: 18px;
+    margin-bottom: 2px;
+    }
+  .maplegend ul.legend-labels li span {
+    display: block;
+    float: left;
+    height: 16px;
+    width: 30px;
+    margin-right: 5px;
+    margin-left: 0;
+    border: 1px solid #999;
+    }
+  .maplegend .legend-source {
+    font-size: 80%;
+    color: #777;
+    clear: both;
+    }
+  .maplegend a {
+    color: #777;
+    }
+</style>
+{% endmacro %}"""
+
+macro = MacroElement()
+macro._template = Template(template)
+
+map.get_root().add_child(macro)
 map.add_child(fgv)
 map.add_child(fgp)
 map.add_child(folium.LayerControl())
+
 
 
 
